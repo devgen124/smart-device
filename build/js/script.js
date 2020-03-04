5975ca6a942bb1149603e4f292934c6a18498497
Evgeniy Kolegov <jpriest1987@gmail.com>
2020-03-04 14:39:51 +0700

    footer доделан, фикс с заменой текста в кнопке promo

--- build/js/script.js
+++ build/js/script.js
@@ -1,13 +1,19 @@
 'use strict';
-var html = document.querySelector('html');
-var windowWidth = html.clientWidth;
-var MOBILE_WIDTH = 767;
 
-var promoBtn = document.querySelector('.button--big');
+var footer = document.querySelector('.page-footer');
+var cols = footer.querySelectorAll('.page-footer__col');
+// var togglers = footer.querySelectorAll('.page-footer__toggle');
+// var lists = footer.querySelectorAll('.page-footer__list');
+footer.classList.remove('page-footer--nojs');
 
-function changeText() {
-  if (windowWidth <= MOBILE_WIDTH) {
-    promoBtn.textContent = 'Бесплатная консультация';
-  }
-}
-changeText();
+cols.forEach(function (col) {
+  var btn = col.querySelector('.page-footer__toggle');
+  var list = col.querySelector('.page-footer__list');
+  list.classList.remove('page-footer__list--show');
+
+  btn.addEventListener('click', function () {
+    list.classList.toggle('page-footer__list--show');
+    btn.classList.toggle('page-footer__toggle--plus');
+  });
+});
+
