211c673b507d2f50bffe041438e100a7aec42ac8
Evgeniy Kolegov <jpriest1987@gmail.com>
2020-03-04 11:25:59 +0700

    footer tablet

--- build/js/script.js
+++ build/js/script.js
@@ -2,8 +2,12 @@
 var html = document.querySelector('html');
 var windowWidth = html.clientWidth;
 var MOBILE_WIDTH = 767;
+
 var promoBtn = document.querySelector('.button--big');
 
-if (windowWidth < MOBILE_WIDTH) {
-  promoBtn.textContent = 'Бесплатная консультация';
+function changeText() {
+  if (windowWidth <= MOBILE_WIDTH) {
+    promoBtn.textContent = 'Бесплатная консультация';
+  }
 }
+changeText();
